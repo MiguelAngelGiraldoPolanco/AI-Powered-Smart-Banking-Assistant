97ec8400cca3ab43f4bcddf9e8a48171777d67d0
MiguelAngel <miguelgiraldo1116@gmail.com>
2026-01-18 00:07:58 +0100

    feat(domain):add class client

---
+++ microservicio-cliente/cliente/src/main/java/com/cliente/cliente/domain/Client.java
@@ -0,0 +1,32 @@
+package com.cliente.cliente.domain;
+
+public class Client {
+
+    private String email;
+    private String name;
+    private int phone;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}

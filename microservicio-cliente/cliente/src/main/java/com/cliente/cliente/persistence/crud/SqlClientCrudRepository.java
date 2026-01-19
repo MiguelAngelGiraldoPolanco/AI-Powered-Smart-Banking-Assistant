bb71fe9ba714e13118cbee40547d2ca6338a0770
MiguelAngel <miguelgiraldo1116@gmail.com>
2026-01-19 16:39:08 +0100

    fix(persistence\crud):correct ID type in  SqlClientCrudRepository to mach Client entity

--- microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/crud/SqlClientCrudRepository.java
+++ microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/crud/SqlClientCrudRepository.java
@@ -3,5 +3,5 @@ package com.cliente.cliente.persistence.crud;
 import com.cliente.cliente.persistence.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SqlClientCrudRepository extends JpaRepository<Client,Integer>{
+public interface SqlClientCrudRepository extends JpaRepository<Client,String>{
 }

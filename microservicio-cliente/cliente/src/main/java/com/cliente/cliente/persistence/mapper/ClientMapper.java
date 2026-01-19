de98afa8e40cd4ce05e41be6357fe9543792ceee
MiguelAngel <miguelgiraldo1116@gmail.com>
2026-01-19 17:00:41 +0100

    feat(persistence\mapper): add ClientMapper

--- microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/mapper/ClientMapper.java
+++ microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/mapper/ClientMapper.java
@@ -1,4 +1,16 @@
 package com.cliente.cliente.persistence.mapper;
 
+import com.cliente.cliente.domain.Client;
+import com.cliente.cliente.persistence.entity.ClientEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ClientMapper {
+
+    // Aquí MapStruct mira el tipo de retorno y el parámetro
+    Client toClient(ClientEntity clientEntity);
+
+    @InheritInverseConfiguration
+    ClientEntity toClientEntity(Client client);
 }

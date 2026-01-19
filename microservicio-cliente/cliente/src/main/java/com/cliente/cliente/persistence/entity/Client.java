88a73e090f9c9968f422c108292357ffa3d60b18
MiguelAngel <miguelgiraldo1116@gmail.com>
2026-01-19 16:08:36 +0100

    feat(persistence\entity): create Client entity

--- microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/entity/Client.java
+++ microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/entity/Client.java
@@ -1,4 +1,49 @@
 package com.cliente.cliente.persistence.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="clients")
 public class Client {
+
+    @Id
+    @Column(name="correo_electronico",nullable = false)
+    private String email;
+
+    @Column(name = "nombre", nullable = false, length = 200)
+    private String name;
+
+    @Column(name = "movil", nullable = false, length = 11)
+    private String phone;
+
+    public Client(){
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }

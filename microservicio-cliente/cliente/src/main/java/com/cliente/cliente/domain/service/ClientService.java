2c8d190fc392d61edeb716d7f4292a6d5fbc41e0
MiguelAngel <miguelgiraldo1116@gmail.com>
2026-01-19 15:30:37 +0100

    feat(domain/service): create ClienteService

---
+++ microservicio-cliente/cliente/src/main/java/com/cliente/cliente/domain/service/ClientService.java
@@ -0,0 +1,32 @@
+package com.cliente.cliente.domain.service;
+
+import com.cliente.cliente.domain.Client;
+import com.cliente.cliente.domain.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public List<Client> getAll(){
+        return clientRepository.getAll();
+    }
+    public Optional<Client> getClient(String email) {
+        return clientRepository.getClient(email);
+    }
+    public Client save (Client client){
+        return clientRepository.save(client);
+    }
+    public boolean delete(String email){
+        return getClient(email).map(customer -> {
+            clientRepository.delete(email);
+            return true;
+        }).orElse(false);
+    }
+}

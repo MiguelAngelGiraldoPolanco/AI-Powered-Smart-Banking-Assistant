aacbfe1d225eeecd4c807cbd3ca759b3e2a7ac4e
MiguelAngel <miguelgiraldo1116@gmail.com>
2026-01-20 16:36:00 +0100

    refactor(persistence): change name class to MongoClientCrudRepository

--- microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/crud/MongoClientCrudRepository.java
+++ microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/crud/MongoClientCrudRepository.java
@@ -3,5 +3,5 @@ package com.cliente.cliente.persistence.crud;
 import com.cliente.cliente.persistence.entity.ClientEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface SqlClientCrudRepository extends MongoRepository<ClientEntity,String> {
+public interface MongoClientCrudRepository extends MongoRepository<ClientEntity,String> {
 }

6aa4c3d9449225866af92e35430b77e3a1469923
MiguelAngel <miguelgiraldo1116@gmail.com>
2026-01-23 20:35:53 +0100

    refactor: add toDomain method

--- microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/mapper/ClientMapper.java
+++ microservicio-cliente/cliente/src/main/java/com/cliente/cliente/persistence/mapper/ClientMapper.java
@@ -2,6 +2,7 @@ package com.cliente.cliente.persistence.mapper;
 
 import com.cliente.cliente.domain.Client;
 import com.cliente.cliente.persistence.entity.ClientEntity;
+import com.cliente.cliente.web.dto.ClientRequest;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 
@@ -9,10 +10,15 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
+    // Web -> Dominio (Para el POST)
+    Client toDomain(ClientRequest clienteRequest);
 
+    // Infra -> Dominio (Para el Get)
     Client toClient(ClientEntity clientEntity);
     List<Client> toClients(List<ClientEntity> clientEntities);
 
-    @InheritInverseConfiguration
+    // Dominio -> Infra (Para el SAVE)
+    @InheritInverseConfiguration(name = "toClient")
     ClientEntity toClientEntity(Client client);
+
 }
